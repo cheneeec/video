89ea712e2297df9904431b7d1570f995dc62b04c
earnest <332472988@qq.com>
2018-10-25 12:33:51 +0800

    update

--- src/app/item/items/items.component.ts
+++ src/app/item/items/items.component.ts
@@ -1,9 +1,9 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ItemsService} from "./items.service";
 import {ActivatedRoute} from "@angular/router";
-import {debounceTime, distinct, filter, map, switchMap, tap} from "rxjs/operators";
-import {fromEvent, Observable} from "rxjs";
-import {CdkScrollable, ScrollDispatcher} from "@angular/cdk/overlay";
+import {filter, map, switchMap, tap} from "rxjs/operators";
+import {Observable} from "rxjs";
+import {ScrollDispatcher} from "@angular/cdk/overlay";
 import {ResponsePage} from "../../domain/response-page.model";
 
 @Component({
@@ -13,58 +13,50 @@ import {ResponsePage} from "../../domain/response-page.model";
 })
 export class ItemsComponent implements OnInit {
 
-    items$: Observable<object[]>;
+    responsePage$: Observable<ResponsePage<object>>;
 
-    currentPage$: Observable<number>;
+
+
+    viewContainer = document.getElementsByClassName('view-container').item(0);
+
 
     constructor(private itemsService: ItemsService,
                 private activatedRoute: ActivatedRoute,
-                private scrollDispatcher: ScrollDispatcher,
-                private elementRef: ElementRef) {
+                private scrollDispatcher: ScrollDispatcher) {
     }
 
 
     ngOnInit() {
 
-        const responsePage$ = this.activatedRoute.queryParams
+        this.responsePage$ = this.activatedRoute.queryParams
             .pipe(
                 switchMap(queryParams => this.doHttpRequest(queryParams['page'], queryParams['size'])),
             );
 
-        this.items$ = responsePage$.pipe(map(responsePage => responsePage.content));
-        this.currentPage$ = responsePage$.pipe(map(responsePage => responsePage.number));
 
         this.scrollDispatcher
             .scrolled(800)
             .pipe(
-                map(() => window.scrollY), //取Y值
-                filter(current => current >= document.body.clientHeight - window.innerHeight), //过滤视窗外的滚动
-                // distinct(),//去重
-                map(y => Math.ceil((y + window.innerHeight) / 2727)) //获取页数
+                filter(() => document.documentElement.clientHeight + document.documentElement.scrollTop > this.viewContainer.scrollHeight - 20),
+         /*       switchMap(() => this.currentPage$.pipe(map(currentPage => currentPage + 1))),
+                switchMap(page => this.doHttpRequest(page)),
+                map(pageResponse => pageResponse.content),*/
             )
-            .subscribe((scrollable) => {
-                console.log(document.documentElement.scrollHeight);//代表整个滚动条多长
-                console.log(document.getElementsByTagName('mat-sidenav-content').item(0));
-                console.log(document.getElementsByTagName('mat-sidenav-content').item(0).scrollHeight);
-                console.log("=========")
-
-                console.log(document.getElementsByTagName('mat-sidenav-content').item(0).scrollHeight + 64);
+            .subscribe(page => {
+                console.log(page);
             });
 
     }
 
 
     private doHttpRequest(page?: number, size?: number): Observable<ResponsePage<object>> {
+        console.log(page, 'page');
         return this.itemsService.findAll(
             this.activatedRoute.snapshot.data['category'], {
                 page: page,
                 size: size
             })
     }
-
-
-
-
 
 
 }
